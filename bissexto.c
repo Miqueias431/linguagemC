cb956d47dc0d1794f3bcfc1f5d335d210cbdf26c
Senac <senac@localhost.localdomain>
2024-02-15 17:03:14 -0300

    add código bissexto.c

---
+++ bissexto.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int ano;
+
+    // Solicita ao usuário para inserir o ano
+    printf("Digite um ano: \n");
+    scanf("%d", &ano);
+
+    // Verifica se o ano é bissexto
+    if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+        printf("%d é um ano bissexto.\n", ano);
+    } else {
+        printf("%d não é um ano bissexto.\n", ano);
+    }
+
+    return 0;
+}
