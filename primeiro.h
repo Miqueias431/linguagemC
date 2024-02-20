612fbf2ca68361bbe9ebb70e7f841fec43a2f200
Senac <senac@localhost.localdomain>
2024-02-20 17:20:57 -0300

    Fazendo teste com colecao

---
+++ primeiro.h
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+int quadrado(int x, int y){
+    return x * y;
+}
---
+++ principal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+#include "primeiro.h"
+
+int main(){
+    
+    int rs = quadrado(5,7);
+
+    printf("A area do quadrade de 5 e 7 é: %d\n",rs);
+
+    return 0;
+}
